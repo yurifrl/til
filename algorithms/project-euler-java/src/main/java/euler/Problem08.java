c07ebd1a9da0c73faea843342200d927eaee511d
yuri <yurifl03@gmail.com>
2020-05-29 01:33:24 -0300

    feature: wip :zap:

--- algorithms/project-euler-java/src/main/java/euler/Problem08.java
+++ algorithms/project-euler-java/src/main/java/euler/Problem08.java
@@ -3,16 +3,40 @@ package euler;
 import java.util.LinkedList;
 
 public class Problem08 {
+    // Find the thirteen adjacent digits in the 1000-digit number
+    // that have the greatest product.What is the value of this product?
     public long exec(int[] n, int count) {
-        // LinkedList<Integer> queue = new LinkedList<Integer>();
-        // int[] l = new int[count];
-        // for (int i : n) {
-        //     if (queue.size() > count) {
-        //         queue.removeLast();
-        //     }
-        //     queue.add(i);
-        // }
-        // System.out.println(queue);
-        return 1234;
+        // Create a array for the candidates
+        //   Loaded with the first fourt elements of the array
+        // Create a win variable
+        // with the sum of the four first elements
+        // Sum first four
+        // Loop through the array
+        long pointerProduct = 1,
+             winnerProduct = 1,
+             last = 0;
+        LinkedList<Long> pointer = new LinkedList<Long>();
+        for (long i : n) {
+            // If index is 0
+            if (i == 0) {
+                pointer.clear();
+                pointerProduct = 1;
+                continue;
+            }
+            pointer.addLast(i);
+            pointerProduct = pointerProduct * i;
+            if (pointer.size() <= count) {
+                continue;
+            }
+            // Move pointer, removing last
+            last = pointer.pop();
+            // And always remove last from product
+            pointerProduct = pointerProduct / last;
+
+            if (pointerProduct > winnerProduct) {
+                winnerProduct = pointerProduct;
+            }
+        }
+        return winnerProduct;
     }
 }
