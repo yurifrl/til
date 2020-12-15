b001ba259f3c554faa4aca6bc89303b722d4dff3
yurifrl <yurifl03@gmail.com>
2020-12-14 21:59:12 -0300

    :zap:

---
+++ adventOfCode/c++/README.md
@@ -0,0 +1,11 @@
+# Advent Of Code 2020
+
+```
+$ cd build
+$ cmake ..
+$ make
+```
+
+## Reference
+- [cmake examples](https://github.com/ttroy50/cmake-examples)
+- [cmake example, dont look at the code ;D](https://github.com/Kazhuu/advent-of-code-2020/blob/main/CMakeLists.txt)
--- adventOfCode/c++/day_02.cpp
+++ adventOfCode/c++/day_02.cpp
@@ -1,46 +1,100 @@
+#include <fstream>
 #include <iostream>
-#include <string>
+using namespace std;
 
 class Input {
-  public:
-    int lowContranit;
-    int topContranit;
-    char constraint;
-    std::string word;
-    Input( int a, int b, char c, std::string d) {
-			lowContranit = a;
-			topContranit = b;
-      constraint = c;
-      word = d;
-		}
-    bool isValid() {
-      int count = 0;
-      for (int i = 0; i< word.length(); i++) {
-        if (word[i] == constraint) {
-          count++;
-        }
-        if (count > topContranit) return false;
-      } 
-      if (count < lowContranit) return false;
-      return true;
-		}
+public:
+  int lowContranit;
+  int topContranit;
+  char constraint;
+  string word;
+  Input(int a, int b, char c, std::string d) {
+    lowContranit = a;
+    topContranit = b;
+    constraint = c;
+    word = d;
+  }
+  bool isValid() {
+    int count = 0;
+    for (int i = 0; i < word.length(); i++) {
+      if (word[i] == constraint) {
+        count++;
+      }
+      if (count > topContranit)
+        return false;
+    }
+    if (count < lowContranit)
+      return false;
+    return true;
+  }
+  bool isTwoValid() {
+    if (word[lowContranit] == constraint && word[topContranit] == constraint) { return false; };
+    return word[lowContranit] == constraint|| word[topContranit] == constraint;
+  }
 };
 
-
-int main() {
+void sample() {
+  int result = 0;
   const int input_size = 3;
   bool inputs[input_size] = {
-    Input (1 , 3, 'a', "abcde").isValid(),
-    Input (1 , 3, 'b', "cdefg").isValid(),
-    Input (2 , 9, 'c', "ccccccccc").isValid(),
+      Input(1, 3, 'a', "abcde").isValid(),
+      Input(1, 3, 'b', "cdefg").isValid(),
+      Input(2, 9, 'c', "ccccccccc").isValid(),
   };
 
-
-
-  int result = 0;
   for (int i; i < input_size; i++) {
-    if (inputs[i] == false) result++;
+    if (inputs[i])
+      result++;
+  }
+
+  std::cout << "sample 01: " << result << std::endl;
+}
+
+void exer_01() {
+  int result = 0;
+
+  ifstream infile;
+  string input;
+  infile.open("../data/day_02.txt");
+
+  while (getline(infile, input)) {
+    char w[100];
+    Input i(0, 0, 0, "");
+    sscanf(input.c_str(), "%d-%d %c: %s", &i.lowContranit, &i.topContranit,
+           &i.constraint, w);
+    i.word.append(w);
+
+    if (i.isValid())
+      result++;
   }
 
   std::cout << "Result 01: " << result << std::endl;
+}
+
+void exer_02() {
+  int result = 0;
+
+  ifstream infile;
+  string input;
+  char constraint;
+  int min, max;
+  char w[100];
+  infile.open("../data/day_02.txt");
+
+  while (getline(infile, input)) {
+    sscanf(input.c_str(), "%d-%d %c: %s", &min, &max, &constraint, w);
+
+    Input i(min - 1, max - 1, constraint, string(w));
+
+    if (i.isTwoValid())
+      result++;
+  }
+
+  std::cout << "Result 02: " << result << std::endl;
+}
+
+int main() {
+  sample();
+  exer_01();
+  exer_02();
 }
